15419b8242180d0a92c981252c0c137a0edaf168
Shakya Majumdar <shakyathegreat.majumdar@gmail.com>
2021-03-10 13:01:26 +0530

    add timeout

--- bot/exts/tictactoe.py
+++ bot/exts/tictactoe.py
@@ -1,9 +1,12 @@
+import datetime
 from typing import Union, Optional
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 NUMBER_EMOJIS = {i: f"{i}\N{COMBINING ENCLOSING KEYCAP}" for i in range(1, 10)}
+
+MOVE_TIME_LIMIT = 20
 
 
 class TicTacToe(commands.Cog):
@@ -25,6 +28,8 @@ class TicTacToe(commands.Cog):
         self.next_turn: Optional[tuple[str, str]] = None
 
         self.board_embed_message: Optional[discord.Message] = None
+
+        self.last_move_timestamp = None
 
     def _get_board_embed(self):
         s = ""
@@ -58,19 +63,27 @@ class TicTacToe(commands.Cog):
 
         self.next_turn = self.cross_player, "X"
 
-        embed_message = await ctx.send(embed=self._get_board_embed())
+        embed_message = await ctx.send(
+            content=f"It's your turn now, {self.next_turn[0].mention}!",
+            embed=self._get_board_embed(),
+        )
         self.board_embed_message = embed_message
 
         for emoji in NUMBER_EMOJIS.values():
             await embed_message.add_reaction(emoji)
 
+        self.last_move_timestamp = datetime.datetime.now()
+        self._check_time_out.start()
+
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction: discord.Reaction, player: discord.Member):
+
+        self.last_move_timestamp = datetime.datetime.now()
 
         if player == self.bot.user:
             return
 
-        await reaction.remove(player)
+        # await reaction.remove(player)
 
         if (
             not self.game_in_play
@@ -93,12 +106,15 @@ class TicTacToe(commands.Cog):
 
         self.board[(pos - 1) // 3][(pos - 1) % 3] = self.next_turn[1]
 
-        await self.board_embed_message.edit(embed=self._get_board_embed())
-
         self.next_turn = (
             (self.cross_player, "X")
             if self.next_turn[1] == "O"
             else (self.naught_player, "O")
+        )
+
+        await self.board_embed_message.edit(
+            content=f"It's your turn now, {self.next_turn[0].mention}!",
+            embed=self._get_board_embed(),
         )
 
         status = self._get_game_status()
@@ -116,7 +132,8 @@ class TicTacToe(commands.Cog):
         else:
             await self.board_embed_message.channel.send("Game ended in a draw!")
 
-        await self.board_embed_message.clear_reactions()
+        self._check_time_out.cancel()
+        # await self.board_embed_message.clear_reactions()
         self._reset_game()
 
     def _get_game_status(self):
@@ -152,6 +169,23 @@ class TicTacToe(commands.Cog):
                     wins_right_diagonal = False
 
         return any((wins_left_diagonal, wins_right_diagonal))
+
+    @tasks.loop(seconds=1)
+    async def _check_time_out(self):
+        if not self.game_in_play:
+            return
+        lapsed = datetime.datetime.now() - self.last_move_timestamp
+        if lapsed.seconds > MOVE_TIME_LIMIT:
+            await self.board_embed_message.channel.send(
+                f"{self.next_turn[0].mention} your time is up! Ending game now."
+            )
+            self._check_time_out.cancel()
+            # await self.board_embed_message.clear_reactions()
+            self._reset_game()
+        elif lapsed.seconds == MOVE_TIME_LIMIT - 5:
+            await self.board_embed_message.channel.send(
+                f"Hurry up {self.next_turn[0].mention}! You have 5 seconds remaining."
+            )
 
 
 def setup(bot: commands.Bot):
