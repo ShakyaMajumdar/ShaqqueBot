c43ac88122527dec7b3327515bc513e1a7005b21
Shakya Majumdar <shakyathegreat.majumdar@gmail.com>
2021-03-10 11:35:09 +0530

    Add reactions to embed automatically

--- bot/exts/tictactoe.py
+++ bot/exts/tictactoe.py
@@ -3,17 +3,7 @@ from typing import Union, Optional
 import discord
 from discord.ext import commands
 
-NUMBER_EMOJIS = {
-    1: ":one:",
-    2: ":two:",
-    3: ":three:",
-    4: ":four:",
-    5: ":five:",
-    6: ":six:",
-    7: ":seven:",
-    8: ":eight:",
-    9: ":nine:"
-}
+NUMBER_EMOJIS = {i: f"{i}\N{COMBINING ENCLOSING KEYCAP}" for i in range(1, 10)}
 
 
 class TicTacToe(commands.Cog):
@@ -24,7 +14,9 @@ class TicTacToe(commands.Cog):
     def _reset_game(self):
         self.game_in_play = False
 
-        self.board: list[list[Union[int, str]]] = [[3 * i + 1, 3 * i + 2, 3 * i + 3] for i in range(3)]
+        self.board: list[list[Union[int, str]]] = [
+            [3 * i + 1, 3 * i + 2, 3 * i + 3] for i in range(3)
+        ]
         self.empty_spots: set[int] = set(range(1, 10))
 
         self.naught_player: Optional[discord.Member] = None
@@ -52,6 +44,10 @@ class TicTacToe(commands.Cog):
     @commands.command(name="ttt")
     async def start(self, ctx: commands.Context, member: discord.Member = None):
 
+        if self.game_in_play:
+            await ctx.send("One game is already in progress!")
+            return
+
         """if member == ctx.author:
             raise commands.UserInputError('You can\'t play with yourself.')"""
 
@@ -65,10 +61,22 @@ class TicTacToe(commands.Cog):
         embed_message = await ctx.send(embed=self._get_board_embed())
         self.board_embed_message = embed_message
 
+        for emoji in NUMBER_EMOJIS.values():
+            await embed_message.add_reaction(emoji)
+
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction: discord.Reaction, player: discord.Member):
 
-        if not self.game_in_play or reaction.message != self.board_embed_message or player != self.next_turn[0]:
+        if player == self.bot.user:
+            return
+
+        await reaction.remove(player)
+
+        if (
+            not self.game_in_play
+            or reaction.message != self.board_embed_message
+            or player != self.next_turn[0]
+        ):
             return
 
         try:
@@ -81,23 +89,34 @@ class TicTacToe(commands.Cog):
         else:
             return
 
-        self.board[(pos-1) // 3][(pos-1) % 3] = self.next_turn[1]
+        await reaction.remove(self.bot.user)
+
+        self.board[(pos - 1) // 3][(pos - 1) % 3] = self.next_turn[1]
 
         await self.board_embed_message.edit(embed=self._get_board_embed())
 
-        self.next_turn = (self.cross_player, "X") if self.next_turn[1] == "O" else (self.naught_player, "O")
+        self.next_turn = (
+            (self.cross_player, "X")
+            if self.next_turn[1] == "O"
+            else (self.naught_player, "O")
+        )
 
         status = self._get_game_status()
 
         if status == "in progress":
             return
         elif status == "X":
-            await self.board_embed_message.channel.send(f"{self.cross_player.mention} has won the game!")
+            await self.board_embed_message.channel.send(
+                f"{self.cross_player.mention} has won the game!"
+            )
         elif status == "O":
-            await self.board_embed_message.channel.send(f"{self.naught_player.mention} has won the game!")
+            await self.board_embed_message.channel.send(
+                f"{self.naught_player.mention} has won the game!"
+            )
         else:
-            await self.board_embed_message.channel.send(f"Game ended in a draw!")
+            await self.board_embed_message.channel.send("Game ended in a draw!")
 
+        await self.board_embed_message.clear_reactions()
         self._reset_game()
 
     def _get_game_status(self):
@@ -129,7 +148,7 @@ class TicTacToe(commands.Cog):
                 if i == j and item != piece:
                     wins_left_diagonal = False
 
-                if i+j == 2 and item != piece:
+                if i + j == 2 and item != piece:
                     wins_right_diagonal = False
 
         return any((wins_left_diagonal, wins_right_diagonal))
